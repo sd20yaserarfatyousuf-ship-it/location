0dc4fd7b5f39353e4d45e9efc9278fcaf9ee0c73
sd20yaserarfatyousuf-ship-it <sd20_yaser_arfatyousuf@mgmcen.ac.in>
2025-08-14 18:37:44 +0530

    Update renderapi.py

--- renderapi.py
+++ renderapi.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI, Request
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# 🔁 Replace these with your actual GitHub Pages origins
+# e.g. https://arfat.github.io and/or https://arfat.github.io/location-capture
+origins = [
+    "https://sd20yaserarfatyousuf-ship-it.github.io/location/locatiojn.html",
+    "https://sd20yaserarfatyousuf-ship-it.github.io/location"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+
+
 
 latest_location = {}
 
@@ -15,3 +35,4 @@ async def receive_location(req: Request):
 @app.get("/get-location")
 async def get_location():
     return latest_location
+
